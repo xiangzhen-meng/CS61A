c000912c8cbf0f21c4a1f0f88f49d7f059a05fa0
Xiangzhen Meng <xiangzhenmeng@icloud.com>
2026-01-16 11:23:15 +0800

    hw07

---
+++ hw/hw07/hw07.scm
@@ -0,0 +1,30 @@
+(define (square n) (* n n))
+
+(define (pow base exp) 
+  (cond 
+    ((= exp 0) 1)
+    ((odd? exp) (* base (pow base (- exp 1))))
+    (else (pow (square base) (/ exp 2)))
+  )
+)
+
+(define (repeatedly-cube n x)
+  (if (zero? n)
+    x
+    (let (
+      (y (repeatedly-cube (- n 1) x))
+      )
+      (* y y y)
+    )
+  )
+)
+
+(define (cddr s) (cdr (cdr s)))
+
+(define (cadr s) 
+  (car (cdr s))
+)
+
+(define (caddr s)
+  (car (cdr (cdr s)))
+)
